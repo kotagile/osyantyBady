f0aa6b76579a07d62a126fb999399d60082ea60b
yuka <nagahama.yuka54@gmail.com>
2025-07-30 15:16:31 +0900

    home.jsの追加

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -7,4 +7,20 @@ function startWorkout() {
 document.addEventListener('DOMContentLoaded', function() {
     // 必要に応じて初期化処理を追加
     console.log('ホーム画面が読み込まれました');
-}); 
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+	const goalToggle = document.getElementById("goalToggle");
+	const goalContent = document.getElementById("goalContent");
+	
+	goalToggle.addEventListener("click", function() {
+		goalContent.classList.toggle("open");
+		
+		if(goalContent.classList.contains("open")){
+			goalToggle.setAttribute("src", "/img/pulldown-after.png");
+		} else{
+			goalToggle.setAttribute("src", "/img/pulldown-before.png");
+		}
+	});
+
+});
